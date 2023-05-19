14d091e83cd711da1db55c87c45a5a21c4128757
Maryna Markova <markova.maryna@gmail.com>
2023-05-20 00:43:17 +0300

    add file reviewer function

--- reviewer.py
+++ reviewer.py
@@ -1,9 +1,8 @@
 import openai
 from dotenv import load_dotenv
 import os
+import argparse
 
-load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
 PROMPT = """
 You will receive a file's contents as text.
@@ -11,16 +10,36 @@ Generate a code review for file. Indicate what changes should be made to improve
 """
 
 
-filecontent = """
-def myster(x, y):
-    return x ** y
-"""
+def code_review(file_path, model):
+    with open(file_path, "r") as file:
+        content = file.read()
+    generated_code_review = make_code_review_request(content, model)
+    print(generated_code_review)
 
-messages = [
-    {"role": "system", "content": PROMPT},
-    {"role": "user", "content": f"Code review the following file: {filecontent}"},
-]
 
-res = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+def make_code_review_request(filecontent, model):
+    messages = [
+        {"role": "system", "content": PROMPT},
+        {"role": "user", "content": f"Code review the following file: {filecontent}"},
+    ]
 
-print(res["choices"][0]["message"]["content"])
+    res = openai.ChatCompletion.create(model=model, messages=messages)
+
+    return res["choices"][0]["message"]["content"]
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Simple code reviewer for a file")
+    parser.add_argument("file")
+    parser.add_argument("--model", default="gpt-3.5-turbo")
+    args = parser.parse_args()
+    code_review(args.file, args.model)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    main()
+
+
+# cmd python reviewer.py file_name
